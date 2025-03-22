49486e20d6ba2cb21bd8756de1b2716f0be61009
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-03-22 12:15:43 +0530

    Refactor About page layout and content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,28 @@
 export const About = () => {
   return (
     <>
-      <h1>About</h1>
+      <div className="container">
+        <h2 className="text-center py-3">About Us</h2>
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-body">
+                <p className="card-text">
+                  At our company, we specialize in delivering innovative and
+                  high-quality technology solutions tailored to meet the unique
+                  needs of each client. With expertise in web development, app
+                  development, iOS development, software solutions, and UI/UX
+                  design, we focus on creating impactful digital experiences
+                  that drive growth and success. Our team of dedicated
+                  professionals works collaboratively to turn your ideas into
+                  reality, ensuring scalable, efficient, and user-centered
+                  solutions that enhance your business performance.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
