10604095e4405f622af06c55b5970b9a10f97443
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-03-22 13:17:26 +0530

    Added Bootstrap JavaScript and CSS, carousel component, and images to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,64 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { NavLink } from "react-router-dom";
+import "./Home.css";
+import image1 from "../../public/images/code1.jpg";
+import image2 from "../../public/images/code2.jpg";
+import image3 from "../../public/images/code3.jpg";
 
 export const Home = () => {
   return (
     <>
       <div>
+        <div id="carouselExample" className="carousel slide">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src={image1}
+                className="d-block w-100 img-fluid"
+                alt="image.jpg"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src={image2}
+                className="d-block w-100 img-fluid"
+                alt="image.jpg"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src={image3}
+                className="d-block w-100 img-fluid"
+                alt="image.jpg"
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
         <h1 className="text-center py-3">Welcome to N-Tech Global</h1>
 
         <section className="mt-3">
