9f9beed7b35ca9787ad6afe60e652db67ab4428c
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-03-22 11:46:51 +0530

    Removed redundant registration form in Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -82,46 +82,6 @@ export const Register = () => {
           </button>
         </form>
       </div>
-
-      {/* <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username*"
-          value={user.username}
-          onChange={handleInputChange}
-          required
-        />
-        <br />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email*"
-          value={user.email}
-          onChange={handleInputChange}
-          required
-        />
-        <br />
-        <input
-          type="tel"
-          name="phone"
-          placeholder="Phone*"
-          value={user.phone}
-          onChange={handleInputChange}
-          required
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password*"
-          value={user.password}
-          onChange={handleInputChange}
-          required
-        />
-        <br />
-        <button type="submit">Register</button>
-      </form> */}
     </>
   );
 };
